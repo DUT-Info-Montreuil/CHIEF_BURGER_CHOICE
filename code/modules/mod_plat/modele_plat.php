eb32b27921681849e8b62c0a2d4c831db7114aba
bseydi <boulaye.seydi@gmail.com>
2022-11-09 10:22:03 +0100

    modification frauduleuse de modele connexion

--- code/modules/mod_plat/modele_plat.php
+++ code/modules/mod_plat/modele_plat.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once "connexion.php";
 
 class ModelePlat extends Connexion{
@@ -10,11 +9,10 @@ class ModelePlat extends Connexion{
 
 	public function inserer_plat() {
             
-		/*$sql = 'SELECT * FROM Menu';	
+		$sql = 'SELECT * FROM Menu';	
 		$num = 0;
 		foreach (self::$bdd ->query($sql) as $row) {
 			$num++;
-
 		}	  
 		
         $id_menu = $num+1;
@@ -23,41 +21,8 @@ class ModelePlat extends Connexion{
 		$boisson = $_POST['boisson'];
 
 		$sth = self::$bdd->prepare('insert into Menu (id_menu,nom_menu,burger,boisson) values (?,?,?,?)');
-
-		}
-		$sqlburger = 'SELECT * FROM Burger';
-		$sqlboisson = 'SELECT * FROM Boisson';
-
-
-		foreach (self::$bdd ->query($sqlburger) as $row) {
-			if ($row['nom'] == $_POST['burger']) {
-				$prix = $row['prix'];
-				$id_burger = $row['id_burger'];
-			}
-		}
-		foreach (self::$bdd ->query($sqlburger) as $row) {
-			if ($row['boisson'] == $_POST['boisson']) {
-				$id_boisson = $row['id_boisson'];
-			}
-		}
-		
-		$mssmmz = 'SELECT prix FROM Burger where nom = '. $_POST['burger'];
-
-        $id_menu = $num+1;
-		$nom = "Menu ".$_POST['burger'];
-
-		$email;
-		$auteur;
-		
-		$id_boisson = $_POST['boisson'];
-		$id_commande = $_POST['burger'];
-		
-
-		$sth = self::$bdd->prepare('insert into Menu (id_menu,nom,prix,email,auteur,id_burger,id_boisson,id_commande) values (?,?,?,?,?,?,?,?)');
-
 		$sth->execute(array($id_menu,$nomMenu,$burger,$boisson));
-        print "Menu commandé";*/
+        print "Menu commandé";
     }   
 }
-
 ?>
