abc42f9db1db9bd5eaec5ec032bb067898a19c54
Nono78hls <njouini@iut.univ-paris8.fr>
2022-11-16 12:41:55 +0100

    mail marche

---
+++ code/test_mail.php
@@ -0,0 +1,18 @@
+<?php
+$dest="testchiefbc@gmail.com";
+$objet="Bienvenue dans notre restaurant";
+$message="
+    <font face='arial'>
+    Bonjour et bienvenue.n
+    Pour valider votre inscription vous devez écrire le code suivant 
+    </font>
+";
+$entetes="Content-Type: text/html; charset=iso-8859-1";
+$entetes.="From: chiefburgerchoice@gmail.com";
+
+if(mail($dest,$objet,$message,$entetes))
+    echo "Mail envoyé avec succès.";
+else
+    echo "Un problème est survenu.";
+//mail($dest , $objet , $message , $entetes);
+?>
