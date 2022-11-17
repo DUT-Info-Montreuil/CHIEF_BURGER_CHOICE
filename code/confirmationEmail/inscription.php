d0d7580ddb035b96690ce32d34e9a0bb4a2496c3
Nono78hls <njouini@iut.univ-paris8.fr>
2022-11-17 16:10:45 +0100

    debut envoie code confirmation

---
+++ code/confirmationEmail/inscription.php
@@ -0,0 +1,8 @@
+
+<form method="POST" action="">
+    <input type="text" name="nom" placeholder="Entrez votre nom" required>
+    <input type="email" name="email" placeholder="Entrez votre adresse email" required>
+    <input type="password" name="mdp" placeholder="Entrez votre mot de passe" required>
+    <input type="submit" name="inscription" placeholder="Inscription" required>
+</form>
+
