b09802c7b7cbca84e77bd0850c5cf3a78ac63303
bseydi <boulaye.seydi@gmail.com>
2022-12-01 17:45:51 +0100

    Mise à jour de la page d'accueil et correctif sur le formulaire d'inscription.

--- code/modules/mod_connexion/vue_connexion.php
+++ code/modules/mod_connexion/vue_connexion.php
@@ -9,17 +9,9 @@ class VueConnexion extends VueGenerique1{
 		parent::__construct();
 	}
 
-	public function menu() {		
-			$url1 = "index.php?module=mod_connexion&action=inscription";
-			$url2 = "index.php?module=mod_connexion&action=connecter";			
-			echo '<a href="'.$url1.'">'."inscription ".'</a>'.'<br>';
-			echo '<a href="'.$url2.'">'."connexion ".'</a>'.'<br>';	
-			echo '<p><a href="index.php?module=mod_connexion&action=deconnecter">Se déconnecter</a></p>';
-	}
-
     public function form_inscription() {
 		if(isset($_SESSION['log'])) {
-			echo '<div class="formInscription">
+			echo '<div class="formConnexion">
 				<p>Vous êtes déjà connecté sous : ';echo $_SESSION['log'];echo'</p></br>
 				</br>
 				<p><a href="index.php?module=mod_connexion&action=deconnecter">Se déconnecter</a></p></br>
@@ -27,7 +19,7 @@ class VueConnexion extends VueGenerique1{
 		}
 		else {
 			echo'<div class="formConnexion">
-					<form action= "index.php?module=mod_connexion&action=connexion" method="POST">
+					<form action= "index.php?module=mod_connexion&action=sinscrire" method="POST">
 						<div class="form-group">
 							<label for="exampleInputEmail1">Identifiant</label>
 							<input type="text" class="form-control" placeholder="Enter votre nom d\'utilisateur" name= "login">
@@ -35,7 +27,7 @@ class VueConnexion extends VueGenerique1{
 
 						<div class="form-group">
 							<label for="exampleInputPassword1">Email</label>
-							<input type="password" class="form-control" placeholder="Entrez votre email" name= "mail">
+							<input type="text" class="form-control" placeholder="Entrez votre email" name= "mail">
 							<small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais votre e-mail avec quelqu\'un d\'autre.</small>
 						</div>
 
@@ -49,25 +41,9 @@ class VueConnexion extends VueGenerique1{
 							<input type="password" class="form-control" placeholder="Confirmez votre mot de passe" name= "confirmPassword">
 						</div>
 
-						<button type="submit" class="btn btn-primary" name="seConnecter">Submit</button>
+						<button type="submit" class="btn btn-primary" name="inscrire">Envoyer</button>
 					</form>
 				</div>';
-			
-
-        /*echo'<div class="formInscription">
-				<form action= "index.php?module=mod_connexion&action=sinscrire" method="POST">
-					<p>Inscrivez-vous</p></br>
-					<input type= "text" placeholder="Entrez un nom d\'utilisateur" name= "login"></br>
-					</br>
-					<input type= "text" placeholder="Entrez une adresse mail" name= "mail"></br>
-					</br>
-					<input type= "password" placeholder="Entrez un nouveau mot de passe" name= "password"></br>
-					</br>
-					<input type= "password" placeholder="Confirmation du mot de passe" name= "confirmPassword"></br>
-					</br>
-					<input type="submit" name="inscrire" />
-				</form>
-		</div>';*/
 		}
     }
 
@@ -90,11 +66,7 @@ class VueConnexion extends VueGenerique1{
 							<label for="exampleInputPassword1">Mot de passe</label>
 							<input type="password" class="form-control" placeholder="Entrez votre mot de passe" name= "password">
 						</div>
-						<div class="form-group form-check">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Check me out</label>
-						</div>
-						<button type="submit" class="btn btn-primary" name="seConnecter">Submit</button>
+						<button type="submit" class="btn btn-primary" name="seConnecter">Envoyer</button>
 					</form>
 				</div>';
 		}
@@ -104,8 +76,9 @@ class VueConnexion extends VueGenerique1{
 		print "deconnectez-vous";
 		echo'<div class="formConnexion">		
 			<form action= "index.php?module=mod_connexion&action=deconnexion" method="POST">
-				<input type="submit" name="seDeconnecter" />
+				<button type="submit" class="btn btn-primary" name="seDeconnecter">Déconnecter</button>
 			</form>
+			<button type="submit" class="btn btn-primary" name="seDeconnecter">NON</button>
 		</div>';
 	}
 }
